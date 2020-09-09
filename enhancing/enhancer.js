726dcd43b307997394db409da16ad638171af6cf
ClydeFrog04 <egan.randal@gmail.com>
2020-09-08 19:30:46 -0600

    tests created

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,28 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+    success,
+    fail,
+    repair,
+    get,
 };
 
 function success(item) {
-  return { ...item };
+    const newEnhancementLvl = item.enhancement < 20 ? item.enhancement + 1 : 20;
+    return {...item, enhancement: newEnhancementLvl};
 }
 
 function fail(item) {
-  return { ...item };
+    //the rules do NOT specify stopping at zero, so I am allowing negative values
+    let newDurability = item.durability - 1;//set the base case(numbers greater than 16
+    if(item.enhancement < 15) newDurability = item.durability - 5;//first check and reassignment
+    else if(item.enhancement === 15 || item.enhancement === 16) newDurability = item.durability - 10;//second check and reassignment
+
+    return {...item, durability: newDurability};
 }
 
 function repair(item) {
-  return { ...item };
+    return {...item, durability: 100};
 }
 
 function get(item) {
-  return { ...item };
+    return {...item};
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("Repairs item", () => {
+    it("Should set items durability to 100", () => {
+        const item = {enhancement: 10, durability: 20};
+        const repairedItem = enhancer.repair(item);
+        expect(repairedItem.durability).toBe(100);
+    });
+});
+
+describe("Enhances item", function () {
+    // on success enhancement increases by one limiting at 20
+    //on fail enhancement as e < 15 e-= 5 || e===15 e===16 e-=10 || e-=1\
+    let item = {enhancement: 10, durability: 100};
+    beforeAll(() =>{
+        item = {enhancement: 10, durability: 100};
+    });
+    it("Should succeed in enhancement setting enhancement to 11", () =>{
+        const newItem = enhancer.success(item);
+        expect(newItem.enhancement).toBe(11);
+    });
+
+});
